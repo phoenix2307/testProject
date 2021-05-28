8a9f846c1cf8958e1f8d54748de985b347cda8e9
Alex <phoenix.trade2307@gmail.com>
2021-05-28 23:33:11 +0300

    28_05

--- js/scriptTest.js
+++ js/scriptTest.js
@@ -20,3 +20,18 @@ for (let i = 1; i < 8; i++) {
   }
   console.log(i);
 }
+
+function createCounter() {
+  let counter = 0;
+  const myFunction = function () {
+    counter = counter + 1;
+    return counter;
+  };
+
+  return myFunction;
+}
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+console.log('example increment', c1, c2, c3);
