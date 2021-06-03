905b2b66c7ce95ae5f22f73182efdee60c68df05
Alex <phoenix.trade2307@gmail.com>
2021-06-03 23:31:42 +0300

    03_06_21

--- js/scriptTest.js
+++ js/scriptTest.js
@@ -1,34 +1,3 @@
 'use strict';
 
-//
-// PROTOtype
-
-console.dir([1, 2, 3, 'a', 'l']);
-
-const soldier = {
-  health: 400,
-  armor: 100,
-  sayHello: function () {
-    console.log('Hello!');
-  }
-};
-
-const john = {
-  health: 100
-};
-
-//john.__proto__ = soldier; // такой синтаксис не используется
-
-//Современные команды:
-// Object.create(); - создание объекта с определенным прототипом
-// Object.getPrototypeOf(); - получение этого прототипа
-// Object.setPrototypeOf(); - установка этого прототипа
-
-Object.setPrototypeOf(john, soldier); // то же самое, что и john.__proto__ = soldier;
-
-console.log(john.armor);
-john.sayHello();
-console.log(john);
-
-const alex = Object.create(soldier);
-alex.sayHello();
+console.log("Ёжик" < "яблоко");
