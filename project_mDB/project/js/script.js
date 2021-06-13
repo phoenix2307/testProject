36bb933caf84a5219d7c6db66d76d9d9dd4355c2
Alex <phoenix.trade2307@gmail.com>
2021-06-13 14:33:38 +0300

    список фильмов - окончено

--- project_mDB/project/js/script.js
+++ project_mDB/project/js/script.js
@@ -14,27 +14,91 @@ document.addEventListener('DOMContentLoaded', () => {
     const adv = document.querySelectorAll('.promo__adv img'),
         poster = document.querySelector('.promo__bg'),
         genre = poster.querySelector('.promo__genre'),
-        movieList = document.querySelector('.promo__interactive-list');
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
 
-    adv.forEach(item => {
-        item.remove();
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // убрать перезагрузку страницы при нажатии на кнопку (браузер по умолчанию)
+
+        let newFilm = addInput.value;
+        // необходим let, потому что в проверке н адлину строки будет изменяться содержание newFilm (три точки)
+        const favorite = checkbox.checked;
+        // проверка на заполненое поле ввода
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 21)}...`;
+            }
+
+            if (favorite) {
+                console.log('Добавлен любимый фильм');
+            }
+
+            // добавление фильма в массив movieDB.movies
+            movieDB.movies.push(newFilm);
+            // сортировка в массиве по алфавиту (играет роль большие или маленькие буквы)
+            sortArr(movieDB.movies);
+            // создаем новый список фильмов и выводим его на монитор
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        // очистка формы input после добавления фильма
+        event.target.reset();
+
     });
 
-    genre.textContent = 'драма';
+    // функция удаления рекламных блоков
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
 
-    poster.style.backgroundImage = 'url("img/bg.jpg")';
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
 
-    movieList.innerHTML = '';
+    const sortArr = (arr) => {
+        arr.sort();
+    };
 
-    movieDB.movies.sort();
+    function createMovieList(films, parent) {
+        // films - массив фильмов, которые мы будем добавлять и сортировать
+        // parent - какой родительский блок будет использовать эти films
+        parent.innerHTML = '';
+        // movieList.innerHTML = '';
+        sortArr(films);
 
-    movieDB.movies.forEach((film, index) => {
-        movieList.innerHTML += `
-      <li class="promo__interactive-item">${index + 1} ${film}
-          <div class="delete"></div>
-      </li>
-      `;
-    });
+        films.forEach((film, index) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${index + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        // получаем все корзины
+        document.querySelectorAll('.delete').forEach((btn, index) => {
+            // index - порядковый нормер элемента, который удаляется
+            btn.addEventListener('click', () => {
+                // удаление родительского элемента из верстки
+                btn.parentElement.remove();
+                // удаление фильма из массива
+                films.splice(index, 1); // можно использовать films
+                createMovieList(films, parent);
+            });
+        });
+
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    // при загрузке страницы нужно отоюразить стартовый список фильмов - вызываем функцию createMovieList
+    createMovieList(movieDB.movies, movieList);
+
 });
 
 
