1f2eddccc22dd675e821448d69abb043ff2fb753
Alex <phoenix.trade2307@gmail.com>
2021-06-13 23:09:56 +0300

    defer_async

--- js/scriptTest.js
+++ js/scriptTest.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log("Ёжик" < "яблоко");
+const p = document.querySelectorAll('p');
+console.log(p);
+
+// динамическое добавление скрипта на страницу
+
+function loadScript(src) {
+  const script = document.createElement('script.test'); //создание
+  script.src = src; // пусть скрипта, откуда он будет подгружаться
+  script.async = false;
+  document.body.append(script); // добавление в конец body
+}
